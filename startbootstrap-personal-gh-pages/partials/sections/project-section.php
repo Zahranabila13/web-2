bcf9fea7ae8b31cea7cb3ce789d109f1d697386e
Zahranabila13 <fajriyanizahranabila@gmail.com>
2025-03-13 08:38:26 +0700

    praktikum03

---
+++ startbootstrap-personal-gh-pages/partials/sections/project-section.php
@@ -0,0 +1,20 @@
+<section class="py-5">
+                <div class="container px-5 mb-5">
+                    <div class="text-center mb-5">
+                        <h1 class="display-5 fw-bolder mb-0"><span class="text-gradient d-inline">Projects</span></h1>
+                    </div>
+                    <div class="row gx-5 justify-content-center">
+                        <div class="col-lg-11 col-xl-9 col-xxl-8">
+                            <!-- Project Card 1-->
+                            <div class="card overflow-hidden shadow rounded-4 border-0 mb-5">
+                                <div class="card-body p-0">
+                                    <div class="d-flex align-items-center">
+                                        <div class="p-5">
+                                            <h2 class="fw-bolder">Aplikasi Rental Mobil</h2>
+                                            <p>Saya pernah mengembangkan sebuah projek aplikasi rental mobil, yang dirancang untuk mempermudah pengguna dalam menyewa kendaraan secara praktis dan efisien melalui platform digital. </p>
+                                        </div>
+                                        <img class="img-fluid" src="assets/p1.png" alt="..." />
+                                    </div>
+                                </div>
+                            </div>
+            </section>
